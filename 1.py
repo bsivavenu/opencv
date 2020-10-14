c3d1d82edaed48b5e470b8fe45e889b488891ccc
bsivavenu <blue.black83@gmail.com>
2020-10-14 15:08:19 +0530

    UPDATED CODE

---
+++ 1.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+
+# cap = cv2.VideoCapture('demo2.mkv')
+
+# while(1):
+
+#     # Take each frame
+#     _, frame = cap.read()
+
+#     # Convert BGR to HSV
+#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+#     # define range of blue color in HSV
+#     lower_blue = np.array([10,100,100])
+#     upper_blue = np.array([60,255,255])
+
+#     # Threshold the HSV image to get only blue colors
+#     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+#     # Bitwise-AND mask and original image
+#     res = cv2.bitwise_and(frame,frame, mask= mask)
+
+#     cv2.imshow('frame',frame)
+#     cv2.imshow('mask',mask)
+#     cv2.imshow('res',res)
+#     k = cv2.waitKey(5) & 0xFF
+#     if k == 27:
+#         break
+
+# cv2.destroyAllWindows()
+import numpy as np
+from matplotlib import pyplot as plt
+
+# img = cv2.imread('1.jpg',1)
+# ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+# ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+# ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+# ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+# titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+# images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+# for i in range(6):
+#     plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+
+# plt.show()
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('1.jpg',0)
+img = cv2.medianBlur(img,5)
+
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
